8a962b877d04a515c5ece696951e93c5ed9be004
robert-virtual <robertocastill63@gmail.com>
2022-07-20 18:16:16 -0600

    rename files

--- renamefiles.sh
+++ renamefiles.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-i=$2
+if [ -z $1 ]; then
+  echo "must provide a target extension example: \"jpg\"" 
+  exit 
+fi
+
+i=0
+
+if [ -n $2 ]; then
+  i=$2
+fi
+
 for file in *.$1 ; do
   mv $file "$i.$1"
   i=$((i+1))
 done
+
 echo $i
 
 
